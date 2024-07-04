59e52d9c420f2589e7380400bfc339b99c8b63f9
İpek Gültekin <ipek.gultekiin.01@gmail.com>
2024-07-04 23:13:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Figma kullanmak çok zorlayıcı?? ',
+      title: 'Push',
       theme: ThemeData(
         // This is the theme of your application.
         //
