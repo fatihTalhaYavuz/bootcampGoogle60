c1d78fe30e5e39d2f296bddf01dc38804453c4b5
İpek Gültekin <ipek.gultekiin.01@gmail.com>
2024-07-10 21:39:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello Flutter Group 60 ! ',
+      title: 'Hello Flutter Group 60!',
       theme: ThemeData(
         // This is the theme of your application.
         //
